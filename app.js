202887fa9ea82552b3050fb130b300c633e62353
Giladi <erez.giladi@hpe.com>
2016-08-15 18:23:24 +0300

    -

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-// var n = require('child_process').fork('./child.js');
-// n.on('message', function(m) {
-//   console.log('PARENT got message:', m.length);
-// });
-// n.send(new Array(60 * 1000));
-
 "use strict"
 
 let log = (a) => { console.log(a) }
 let toQueryString = (obj) => Object.keys(obj).map( key => key + "=" + obj[key] ).join('&');
-const request = require('request');
 const fs = require('fs');
 const childProcess = require('child_process');
 const THOUSEND = 1000;
 const MILLION = THOUSEND * THOUSEND
 const REQUEST_PARAMS = {
+   // num: 10 * MILLION,
    num: 10 * MILLION,
    min: 1,
-   max: MILLION,
+   max: 1000 * MILLION,
    col: 1,
    base: 10,
    format: 'plain',
@@ -27,7 +21,6 @@ let isEmpty = (arr) => arr.length === 0;
 let append = (xs, ys) => isEmpty(xs) ? ys : xs.concat(ys);
 let smaller = (xs, x) => xs.filter(a => a <= x);
 let larger = (xs, x) => xs.filter(b => b > x);
-let allowedChildren = process.env.CHILD_COUNT || 2;
 
 function generateRandoms(){
    var arr = [];
@@ -40,14 +33,7 @@ function generateRandoms(){
 function getRandomNumbers(){
    return new Promise((resolve, _) => {
       return resolve(generateRandoms());
-      // request(PATH, function (error, response, body) {
-      //   if (error || response.statusCode !== 200) {
-      //     resolve(generateRandoms());
-      //     return;
-      //   }
-      //
-      //   resolve(body.trim().split('\n').map( (x) => parseInt(x) ))
-      // });
+      
    })
 }
 function saveToFile(arr){
@@ -68,26 +54,31 @@ function qsort(arr){
    return ( qsort(smaller(xs, x)) ).concat( [x] ).concat( (qsort(larger(xs, x))) );
 }
 function qsortAsync(arr) {
-   return new Promise((resolve, _) => {
+   return new Promise((resolve, _) => {      
       if (isEmpty(arr)) return resolve([]);
+      if (arr.length <= 5 * MILLION) return resolve(qsort(arr));
 
       let x = arr[0];
       let xs = arr.slice(1);
 
-      allowedChildren = allowedChildren - 2;
+      let smaller_xs = smaller(xs, x);
+      let larger_xs = larger(xs, x)
 
       let p1 = new Promise((resolve, _) => {
-         let child = childProcess.fork('./app.js', [allowedChildren])
+         let child = childProcess.fork('./app.js', [true], { 
+            execArgv: ['--max-old-space-size=4096']
+         });
          child.on('message', resolve);
-         child.send({ data: smaller(xs, x), args: allowedChildren });
+         child.send({ data: smaller_xs});
       });
 
-      allowedChildren = allowedChildren - 2;
 
       let p2 = new Promise((resolve, _) => {
-         let child = childProcess.fork('./app.js', [allowedChildren])
+         let child = childProcess.fork('./app.js', [true], { 
+            execArgv: ['--max-old-space-size=4096']
+         });
          child.on('message', resolve);
-         child.send({ data: larger(xs, x), args: allowedChildren });
+         child.send({ data: larger_xs});
       });
 
 
@@ -98,15 +89,10 @@ function qsortAsync(arr) {
 function runAsChild(){
    process.on('message', function(data){
       console.log("CHILD " + process.pid + " sort " + data.data.length + " numbers");
-      allowedChildren = parseInt(data.args);
 
-      if (allowedChildren > 0 && allowedChildren % 2 == 0) {
-         qsortAsync(data.data).then((res) => {
-            process.send(res);
-         });
-         return;
-      }
-      process.send(qsort(data.data));
+      qsortAsync(data.data).then((res) => {
+         process.send(res);
+      });
    });
 }
 function runAsParent(){
@@ -114,13 +100,24 @@ function runAsParent(){
    let start;
    getRandomNumbers()
       .then((numbers) => {
+         let start = Date.now();
+         numbers.sort();
+
+         log("End Normal sorting " + numbers.length + " numbers in " +  ((Date.now() - start) / 1000 ));
+         let start_again = Date.now();
+         numbers.sort();
+         
+         log("End Second Normal sorting " + numbers.length + " numbers in " +  ((Date.now() - start_again) / 1000 ));
+         return [];
+      })
+      .then((numbers) => {
          start = Date.now();
          return numbers;
       })
       .then(qsortAsync)
-      .then( (sorted) => log("End sorting " + sorted.length + " numbers in " +  (Date.now() - start) / 1000 ) + " secs")
+      .then( (sorted) => log("End sorting " + sorted.length + " numbers in " +  ((Date.now() - start) / 1000 )) )
       .then( () => process.exit(0))
-      .catch( (a,b,c) => log(a + " " + b + " " + c));
+      .catch( (a,b,c) => log(a + " " + b + " " + c) );
 }
 // Main
 var asChild = !!process.argv[2]; // elements 0 and 1 are already populated with env info
